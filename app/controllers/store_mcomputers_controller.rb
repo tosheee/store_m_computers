319ce4971e73a86ae0770891a6b08a5a73a7df39
Todor Chakarov <tosheee@abv.bg>
2017-05-22 21:24:10 +0300

    Fix filters

--- app/controllers/store_mcomputers_controller.rb
+++ app/controllers/store_mcomputers_controller.rb
@@ -4,36 +4,11 @@ class StoreMcomputersController < ApplicationController
     @categories = category
   end
 
-
-  def filters_product(products_all)
-    gen_prod_filter = {}
-    properties = products_all.map { |prod| eval(prod.description)[:properties][:property]}
-    prod_filter_names = eval(products_all.first.description)[:properties][:property].map{|d| d[:name]}
-
-    prod_filter_names.map do |name|
-      if name[/Manufacturer|Screen size|Screen Resolution|Memory size|CPU Model/]
-        gen_prod_filter[name] = properties.map do |property|
-          property.map { |p| clean_value_filter(name, p[:text].squish) if p.is_a?(Hash) && p[:name] == name  }.join
-        end.reject { |c| c.empty? }.uniq.sort
-      end
-    end
-
-    gen_prod_filter
-  end
-
-  def clean_value_filter(name, value)
-    if name[/Manufacturer/]
-      value
-    elsif name[/Screen size/]
-      value[/\d+.\d|\d+/]
-    elsif name[/Screen Resolution/]
-      value.gsub(' ', '')[/[0-9]{4}x[0-9]{3,4}/]
-    elsif name[/Memory size/]
-      value[/\d+\sGB/i].to_s
-    elsif name[/CPU Model/]
-      value[/(AMD|intel)\s(\w\d|core|atom|celeron|dual-core|pentium)\si\d/i]
-    else
-      value
+  def show
+    @currency = currency # method app controller
+    @order_item = current_order.order_items.new
+    unless @products = Admin::ProductFeature.where(id: params[:id]).first
+      render text: "Page not found", status: 404
     end
   end
 
@@ -42,7 +17,6 @@ class StoreMcomputersController < ApplicationController
     @page = params[:page].to_i
     @product_param = params[:product]
     product_num = 10
-
     @prod_filter_names = eval(product_all.where(identifier: params[:product]).first.description)[:properties][:property].map{|d| d[:name]}
 
     if params[:search].present?
@@ -71,20 +45,85 @@ class StoreMcomputersController < ApplicationController
     @order_item = current_order.order_items.new
   end
 
-  def filter_result(param, products)
-    q_product = []
-    products.select do |product|
-      if eval(product.description)[:code].to_s[/#{param}/i]
-        q_product << product
+  def filters_product(products_all)
+    gen_prod_filter = {}
+    properties = products_all.map { |prod| eval(prod.description)[:properties][:property]}
+    prod_filter_names = product_filter_name(products_all)
+
+    prod_filter_names.map do |name|
+      if name[/Manufacturer|Screen size|Screen Resolution|Memory size|CPU Model/]
+        gen_prod_filter[name] = properties.map do |property|
+          property.map { |p| clean_value_filter(name, p[:text].squish) if p.is_a?(Hash) && p[:name] == name  }.join
+        end.reject { |c| c.empty? }.uniq.sort
       end
     end
 
-    if q_product.size > 0
-      q_product
+    gen_prod_filter
+  end
+
+  def clean_value_filter(name, value)
+    if name[/Manufacturer/]
+      value
+    elsif name[/Screen size/]
+      value[/\d+.\d|\d+/]
+    elsif name[/Screen Resolution/]
+      value.gsub(' ', '')[/[0-9]{4}x[0-9]{3,4}/]
+    elsif name[/Memory size/]
+      value[/\d+\sGB/i].to_s
+    elsif name[/CPU Model/]
+      value[/(AMD|intel)\s(\w\d|core|atom|celeron|dual-core|pentium)\si\d/i]
     else
-      flash[:error] = "Няма намерени резултати"
-      products
+      value
     end
+  end
+
+  def product_filter_name(products_all)
+    eval(products_all.first.description)[:properties][:property].map{|d| d[:name]}
+  end
+
+  def filter_result(param, products_data)
+    filter_param = JSON.parse(param.gsub('(', '[').gsub(')', ']'))
+    #products = products_data.map { |d| eval(d.description)[:properties][:property] }
+    filtered_products = []
+
+    unless filter_param.map { |k, v| k[/Manufacturer/] }.join.empty?
+      filter_param.map do |keys, values|
+        products_data.select do |product|
+          eval(product.description)[:properties][:property].each do |product_val|
+            if product_val.is_a?(Hash) &&  product_val[:name][/Manufacturer/i]
+              if product_val[:name][/#{keys}/i]
+                values.map do |value|
+                  removed = /(\(|,).*/i
+                  if product_val[:text].gsub(removed, '')[/#{value}/i]
+                    puts product_val[:text].gsub(removed, '')
+                    filtered_products << product
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    else
+      filter_param.map do |keys, values|
+        products_data.select do |product|
+          eval(product.description)[:properties][:property].each do |product_val|
+            if product_val.is_a?(Hash)
+              if product_val[:name][/#{keys}/i]
+                values.map do |value|
+                  removed = /(\(|,).*/i
+                  if product_val[:text].gsub(removed, '')[/#{value}/i]
+                    puts product_val[:text].gsub(removed, '')
+                    filtered_products << product
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+    filtered_products
   end
 
   def search(search_text, all_products)
@@ -102,14 +141,6 @@ class StoreMcomputersController < ApplicationController
     else
       flash[:error] = "Няма намерени резултати"
       all_products
-    end
-  end
-
-  def show
-    @currency = currency # method app controller
-    @order_item = current_order.order_items.new
-    unless @products = Admin::ProductFeature.where(id: params[:id]).first
-      render text: "Page not found", status: 404
     end
   end
 end
